e1ec8abfae4b7b6a82cc5085330d524be12c3bc2
Axel-Bravo <axel.bravo.mutllo@protonmail.com>
2019-03-30 13:32:57 +0100

    feat: create "corpus process"
    Corpus Process load the text data into a dictionary with a
    pre-processing that:
    - Transform string to lower
    - Remove accents
    - Remove Non alphabetic characters or \n
    - Lemmatizes text
    - Removes stop words (disabled)

--- src/build_data_poems.py
+++ src/build_data_poems.py
@@ -1,173 +1,107 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Mar 23 22:41:47 2019
-
-@author: reynaldo.espana.rey
-
-Build data for song corpus
-
-source: https://www.analyticsvidhya.com/blog/2018/03/text-generation-using-python-nlp/
-"""
-
-
-# =============================================================================
-# Libraries
-# =============================================================================
+#%% Imports and function declaration
 import numpy as np
-import pandas as pd
 import os
 import re
 import pickle
-from sklearn.utils import shuffle 
-from keras.utils import np_utils
+from sklearn.utils import shuffle
+
+import nltk
+import spacy
+import unicodedata
+from nltk.tokenize.toktok import ToktokTokenizer
+tokenizer = ToktokTokenizer()
+stopword_list = nltk.corpus.stopwords.words('spanish')
+nlp = spacy.load('es', parse=False, tag=False, entity=False)
+
+# Parameters
+corpus_path = 'data/DB/spanish poems/'
+output_path = '../data/data_proccessed/'
+output_file = 'npl_words__seq_len_5'
 
 
+def remove_accented_chars(text: str) -> str:
+    """
+    Cleans accented characters from text
+    :param text: text to process
+    :return: text processed
+    """
+    text = unicodedata.normalize('NFKD', text).encode('ascii', 'ignore').decode('utf-8', 'ignore')
 
-# =============================================================================
-# Input arguments
-# =============================================================================
-# is it a test? if True only 25 songs are used
-TEST_MODE = False
-
-# Where is the folder with all the corpus docs?
-CORPUS_PATH = '../data/DB/spanish poems/'
-
-# Split train/test
-SPLIT = .9
-
-# NLP
-MAX_SEQ = 120
-MIN_SEQ = 40
-STRIDE = [4, 12]
-
-# output path
-OUTPUT_PATH = '../data/data_proccessed/'
-OUTPUT_FILE = 'NLP_data_poems_120-40'
+    return text
 
 
+def remove_special_characters(text: str) -> str:
+    """
+    Removes special characters
+    :param text: text to process
+    :return: text processed
+    """
+    for i_pattern in ['\r\r\n', '\r\n ', '\r\n', '\n\n']:  # line space: '\r\n ' '\r\n' to '\n', '\r\r\n'
+        text = re.sub(i_pattern, '\n', text)
+    text = re.sub(' +', ' ', text).strip()
+    text = re.sub('[^a-z\n ]', '', text)
+
+    return text
 
 
-# =============================================================================
-# Read data
-# =============================================================================
-# read song corpus
-def read_corpus(path):
-    # list of files
-    files = os.listdir(path)
-    print('\nReading:', len(files), 'files')
-    # placeholder for results
+def lemmatize_text(text: str, lemmatizer: spacy.lang) -> str:
+    """
+    Group together the inflected forms of a word
+    :param text: text to process
+    :param lemmatizer: Spacy's lemmatizer
+    :return: text processed
+    """
+    text = lemmatizer(text)
+    text = ' '.join([word.lemma_ if word.lemma_ != '-PRON-' else word.text for word in text])
+    return text
+
+
+def remove_stopwords(text: str, tokenizer: nltk.tokenize.toktok.ToktokTokenizer) -> str:
+    """
+    Remove stopwords from text
+    :param text: text to process
+    :param tokenizer: nltk tokenizer
+    :return: text processed
+    """
+    tokens = tokenizer.tokenize(text)
+    tokens = [token.strip() for token in tokens]
+    filtered_tokens = [token for token in tokens if token.lower() not in stopword_list]
+    filtered_text = ' '.join(filtered_tokens)
+
+    return filtered_text
+
+
+def process_corpus(path: str) -> list:
+    """
+    Read the all text files present in the path and applies text cleansing
+    :param path: path where to look for text files
+    :return: corpus of data
+    """
+    files = os.listdir(path)[1:10]
     corpus = []
-    # loop all files and read
+
     for file in files:
         with open(path + file, "rb") as text_file:
-            doc = text_file.read()
-            doc = doc.decode('latin-1')
-            # lower case
-            doc = doc.lower()
-            # remove leading, ending and duplicates whitespaces
-            doc = re.sub(' +', ' ', doc).strip()
-            corpus.append({'file': path + file,
-                           'corpus': doc})
+            document = text_file.read()
+            document = document.decode('latin-1')
+            document = document.lower()
+            document = re.sub(' +', ' ', document).strip()
+            document = remove_accented_chars(text=document)
+            document = remove_special_characters(text=document)
+            document = lemmatize_text(text=document, lemmatizer=nlp)
+            # document = remove_stopwords(text=document, tokenizer=tokenizer)
+            corpus.append({'file': path + file, 'text': document})
+
     return corpus
 
-# read all .txt lyrics files
-corpus = read_corpus(CORPUS_PATH)
 
-# If it is a test mode just take first 25 songs
-if TEST_MODE:
-    print("TEST MODE: ON")
-    corpus = corpus[:25]
-else:
-    print("TEST MODE: OFF")
-
-# =============================================================================
-# Some Stats and Data Cleansing
-# =============================================================================
-
-# Character per doc.
-chars = [len(x['corpus']) for x in corpus]
-print('\nCharacters per doc:\n', pd.Series(chars).describe())
-# remove outliers, less than 260 characters
-idx = [x > 260 for x in chars]
-print('docs removed:', len(corpus) - sum(idx))
-corpus = list(np.array(corpus)[np.array(idx)])
+#%% Data load and pre-processing
+corpus = process_corpus(corpus_path)
 
 
-# lines per doc, remove single liners
-lines = [len(x['corpus'].split('\n')) for x in corpus]
-print('\nLines per doc:\n', pd.Series(lines).describe())
-# remove outliers. less than 10 lines
-idx = [((x > 8) & (x < 50)) for x in lines]
-print('docs removed:', len(corpus) - sum(idx))
-corpus = list(np.array(corpus)[np.array(idx)])
 
 
-# words count.
-words = [len(re.findall(r'\w+', x['corpus'])) for x in corpus]
-print('\nwords per doc:\n', pd.Series(words).describe())
-# remove outliers
-# less than 100 words
-idx = [((x > 30) & (x < 200)) for x in words]
-print('docs removed:', len(corpus) - sum(idx))
-corpus = list(np.array(corpus)[np.array(idx)])
 
-print('\nNew docs in corpus', len(corpus))
-
-# unique characters count. character unique songs
-# put together all the text to take unique characters
-def merge_corpus(corpus):
-    all_text = str()
-    for x in corpus:
-        all_text = all_text + x['corpus']
-    print('Number of characters in corpus:', len(all_text))
-    return all_text
-
-# all songs as single string
-all_text = merge_corpus(corpus)
-# unique characters
-characters = sorted(list(set(all_text)))
-print('unique characters:', len(characters))
-
-
-# count of characters
-print('Number of appereance per unique character in corpus')
-chars_count = []
-for digit in characters:
-    counter = {'digit': digit,
-                        'count': sum([digit in char for char in all_text])}
-    print(counter)
-    chars_count.append(counter)
-chars_count = pd.DataFrame(chars_count)
-
-# remove non meaningfull characters
-# manually make the cut at 766
-# chars_count['digit'][chars_count['count'] <= 766].values
-chars_remove = ['\t', '"', '%', '&', "'", '\*', '\+', '/', '0', '1', '2', '3', '4',
-                '5', '6', '7', '8', '9', '=', '\[', '\]', '_',  '`', '{',
-                '¨', 'ª', '«', '²', '´', 'º', '»', 'à', 'â', 'ã', 'ä', 'ç',
-                'è', 'ê', 'ì', 'ï', 'ò', 'ô', 'õ', 'ö', 'ü', '\xa0', '°']
-
-print('\nErase from docs non meaningful characters:', chars_remove)
-# as reggex expression
-chars_remove = '[' + ''.join(chars_remove) + ']'
-# erase from docs non meaninful characters
-for doc in corpus:
-    doc['corpus'] = re.sub(chars_remove, ' ', doc['corpus'])
-    doc['corpus'] = re.sub(' +', ' ', doc['corpus']).strip()
-
-
-# line space: '\r\n ' '\r\n' to '\n', '\r\r\n'
-for doc in corpus:
-    for pattern in ['\r\r\n', '\r\n ', '\r\n', '\n\n']:
-        doc['corpus'] = re.sub(pattern, '\n', doc['corpus'])
-
-
-# add special charater for final of text. Model will learn when to end
-for doc in corpus:
-    doc['corpus'] = doc['corpus'] + '.$'
-
-
-# TODO: clean key words
 
 
 # =============================================================================
@@ -209,7 +143,7 @@ words_mapping = {'characters': characters,
 # create corpus index
 idx = [i for i in range(len(corpus))]
 # random random for train
-idx_train = np.random.choice(idx, size=int(len(corpus)*SPLIT), replace=False)
+idx_train = np.random.choice(idx, size=int(len(corpus) * train_test_split), replace=False)
 # index not in train
 idx_test = [i for i in idx if i not in idx_train]
 # split corpus by index
@@ -313,6 +247,6 @@ data = {'corpus': corpus,
         'test_y' : test_y}
 
 # save file
-with open(OUTPUT_PATH + OUTPUT_FILE + '.pickle', 'wb') as file:
+with open(output_path + OUTPUT_FILE + '.pickle', 'wb') as file:
     pickle.dump(data, file, protocol=pickle.HIGHEST_PROTOCOL)
-print('Data saved in:', OUTPUT_PATH + OUTPUT_FILE + '.pickle')
+print('Data saved in:', output_path + OUTPUT_FILE + '.pickle')
